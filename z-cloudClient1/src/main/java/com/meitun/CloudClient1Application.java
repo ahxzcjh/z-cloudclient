e8bc940cb1dd4e899546465d6885368d0d1b8755
wanggang <ahxzcjh@163.com>
2018-08-23 09:57:53 +0800

    111

--- z-cloudClient1/src/main/java/com/meitun/CloudClient1Application.java
+++ z-cloudClient1/src/main/java/com/meitun/CloudClient1Application.java
@@ -2,9 +2,15 @@ package com.meitun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
 public class CloudClient1Application {
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CloudClient1Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudClient1Application.class, args);
